0d80240783c9f28d22bbdd501b7c4099f741896b
Ciaran <ciaranborourke@protonmail.com>
2021-10-26 21:01:06 +0100

    Add missing awesomewm configuration file

---
+++ .config/awesome/clients/apps.lua
@@ -0,0 +1,29 @@
+local awful = require("awful")
+
+local tags = awful.screen.focused().tags
+
+apps = {}
+
+apps.browser = function ()
+    awful.spawn(browser)
+end
+
+apps.browser2 = function ()
+    awful.spawn(browser2)
+end
+
+apps.steam = function ()
+    awful.spawn("steam", { tag = tags[3] })
+end
+
+apps.social = function ()
+    awful.spawn("signal-desktop", { tag = tags[5] })
+    awful.spawn("element-desktop", { tag = tags[5] })
+end
+
+apps.media = function ()
+    awful.spawn("vlc", { tag = tags[4] })
+    awful.spawn("freetube", { tag = tags[4] })
+end
+
+return apps
