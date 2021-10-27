5c8603762c9bae5832c29c2813dcb9d52a9d0820
Ciaran O' Rourke <ciaranborourke@protonmail.com>
2021-10-27 21:35:31 +0100

    Support XDG autostart in AwesomeWM

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -73,4 +73,15 @@ mywibar.init(tags)
 local clients = require("clients")
 clients.init()
 
+-- {{{  Autostart
+
+awful.spawn.with_shell(
+    'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
+    'xrdb -merge <<< "awesome.started:true";' ..
+    -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
+    'dex --environment Awesome --autostart --search-paths "$HOME/.config/autostart"' -- https://github.com/jceb/dex
+)
+
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+
+-- }}}
