d5f8659e3082aaf5a17e8b7909083039f31edc07
Ciarán O' Rourke <ciaranborourke-dev@proton.me>
2023-05-30 12:43:46 +0100

    Add treesitter-context nvim plugin

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+    -- add more treesitter parsers
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "cmake",
+                "cpp",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "vim",
+                "yaml",
+            },
+        },
+        dependencies = {
+            {
+                "nvim-treesitter/nvim-treesitter-context",
+                config = function()
+                    require("treesitter-context").setup({})
+                end,
+            },
+        },
+    },
+}
