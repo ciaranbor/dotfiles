4d6a75a49785e52801862752f6d16f5bdb47972a
Ciarán <ciaranborourke-dev@proton.me>
2024-05-17 16:19:13 +0100

    Add latex nvim plugin

---
+++ .config/nvim/lua/plugins/latex.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "lervag/vimtex",
+        init = function()
+            vim.g.vimtex_view_general_viewer = "zathura"
+            vim.g.vimtex_mappings_enabled = 1
+        end,
+    },
+}
