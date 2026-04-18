3b81ec678bb84e8d689a2efcc3af72fb7525803d
ciaranbor <ciaranborourke-dev@proton.me>
2026-04-18 17:20:23 +0100

    Add nvim binding for dotfiles lazygit

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,15 @@
 return {
     {
         "folke/snacks.nvim",
+        keys = {
+            {
+                "<leader>gr",
+                function()
+                    Snacks.terminal({ "lazygit", "--git-dir=" .. os.getenv("HOME") .. "/.dotfiles", "--work-tree=" .. os.getenv("HOME") }, { cwd = os.getenv("HOME"), esc_esc = false, ctrl_hjkl = false })
+                end,
+                desc = "Lazygit (dotfiles)",
+            },
+        },
         opts = {
             image = { enabled = true },
         },
